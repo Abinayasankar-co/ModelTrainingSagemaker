3e8af983e9e212632db399b9a6945db6aa189a01
Abinayasankar M <122520261+Abinayasankar-co@users.noreply.github.com>
2025-05-10 17:22:03 +0530

    Update inference.py
    changes in the file

--- inference.py
+++ inference.py
@@ -3,20 +3,20 @@ import os
 import pandas as pd
 import json
 
-def model_fn(model_dir):
+def model(model_dir):
     return joblib.load(os.path.join(model_dir, "model.joblib"))
 
-def input_fn(request_body, content_type):
+def input_area(request_body, content_type):
     if content_type == "application/json":
         data = json.loads(request_body)
         return pd.DataFrame(data)
     else:
         raise ValueError(f"Unsupported content type: {content_type}")
 
-def predict_fn(input_data, model):
+def predict_area(input_data, model):
     return model.predict(input_data)
 
-def output_fn(prediction, accept):
+def output_area(prediction, accept):
     if accept == "application/json":
         return json.dumps(prediction.tolist()), accept
     else:
